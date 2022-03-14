dcd782cbfc9c49b2cc232144b3ff1e4ae5700b69
kishanmoliya <94744840+kishanmoliya@users.noreply.github.com>
2022-03-14 09:28:20 +0530

    Add files via upload

---
+++ DBMS/SQLQuery6.sql
@@ -0,0 +1,21 @@
+Select Len('kishan')
+Select Len('   Kishan moliya  ')
+
+Select * From Students
+Where Len(FirstName)>5
+
+Select Len('null')
+
+Select Len(' ')
+
+Select upper('kishan')
+
+Select lower('KISHAN')
+
+Select Left ('kishan',3)
+
+Select right ('kishan',3)
+
+Select SUBSTRING('kishan',3,10)
+
+Select REPLACE('abc123efg','123','XYZ')
