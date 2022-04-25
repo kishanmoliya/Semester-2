f020ca6262d91d2805bee0617e391f4cf718470c
kishanmoliya <kishanmoliya232@gmail.com>
2022-04-25 06:12:20 +0530

    .

--- DBMS/010_LAB_10.sql
+++ DBMS/010_LAB_10.sql
@@ -49,3 +49,79 @@ From Student
 Inner Join Result
 On Student.RNo=Result.RNo
 Group by Branch
+
+
+Select 
+	Student.Branch,AVG(SPI)
+From Student
+Inner Join Result
+On Student.RNo=Result.RNo
+Group by Branch
+Order by avg(SPI) desc
+
+
+Select 
+	Student.Branch,AVG(SPI)
+From Student
+Inner Join Result
+On Student.RNo=Result.RNo
+Where Branch In ('CE','ME')
+Group By Branch
+
+
+Select *
+	From Student
+	Left Outer Join Result
+On Student.RNo=Student.RNO
+	
+
+Select *
+	From Student
+	Right Outer Join Result
+On Student.RNo=Student.RNO
+
+Select *
+	From Student
+	Full Outer Join Result
+On Student.RNo=Student.RNO
+
+
+Select 
+	E.Name,
+	M.Name
+From Employee E
+Right Outer Join Employee M
+On E.EmployeeNo=M.ManagerNo
+
+<----------//////\\\\\\---------->
+
+
+Create Table City
+(
+	CityId int Primary Key Identity(1,1),
+	CityName Varchar(50) Unique,
+	Pincode Varchar(6),
+	Remarks Varchar(250)
+)
+
+Create Table Village
+(
+	VillageId int Primary Key Identity(101,1),
+	VillageName VArchar(50),
+	CityId int Foreign Key References City(CityId)
+)
+
+
+Insert Into City Values('Rajkot','360005','Good')
+Insert Into City Values('Surat','335009','VeryGood')
+Insert Into City Values('Baroda','390001','Awesome')
+Insert Into City Values('Jamnager','361003','Smart')
+Insert Into City Values('Junagath','362229','Historic')
+Insert Into City Values('Morvi','363641','Ceramic')
+
+Insert Into Village Values('Raiya','1')
+Insert Into Village Values('Madhapar','1')
+Insert Into Village Values('Dodka','3')
+Insert Into Village Values('Falla','4')
+Insert Into Village Values('Bhesan','5')
+Insert Into Village Values('Dhoraji','5')
--- DBMS/07_SQLQuery_7.sql
+++ DBMS/07_SQLQuery_7.sql
@@ -30,3 +30,30 @@ Select DATEADD(DAY,30,GETDATE())
 Select DAtEDIFF(YEAR,'2003-06-30',GETDATE())
 Select DAtEDIFF(MONTH,'2003-06-30',GETDATE())
 Select DAtEDIFF(DAY,'2003-06-30',GETDATE())
+
+
+
+
+Create table Student_Marks
+(
+	Sid int,
+	SName Varchar(50),
+	Marks int
+)
+
+Insert Into Student_Marks Values(1,'John',90)
+Insert Into Student_Marks Values(2,'Martin',80)
+Insert Into Student_Marks Values(3,'Carol',89)
+Insert Into Student_Marks Values(4,'Jack',99)
+Insert Into Student_Marks Values(5,'Rose',88)
+Insert Into Student_Marks Values(6,'Mary',90)
+
+Select COUNT(*) As TotalStudent From Student_Marks
+
+Select SUM(Marks) As TotalMarks From Student_Marks
+
+Select AVG(Marks) As AvgMarks From Student_Marks
+
+Select MIN(Marks) As MinMarks From Student_Marks
+
+Select MAX(Marks) As axMarks From Student_Marks
