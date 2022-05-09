37be10ab4b8d566de053f3215b9ff71777310a95
kishanmoliya <94744840+kishanmoliya@users.noreply.github.com>
2022-05-09 11:48:19 +0530

    Update commandsum.java

--- java/01_Basic Programs/commandsum.java
+++ java/01_Basic Programs/commandsum.java
@@ -2,7 +2,7 @@
 public class commandsum {
     public static void main(String[] args){
         double a = Double.parseDouble(args[0]);
-        double b = Double.parseDoubble(args[1]);
+        double b = Double.parseDouble(args[1]);
         double Sum = a+b;
         System.out.println("Sum of two numbers is = "+Sum);
     }
